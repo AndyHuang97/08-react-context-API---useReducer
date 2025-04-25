cda91410286455fc290530f87ab9e54dae3e5dae
Andrea Huang <huangandrea@hotmail.it>
2025-04-25 23:15:45 +0200

    Creating & Providing The Context

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import { CartContext } from "./store/shopping-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -68,7 +69,8 @@ function App() {
   }
 
   return (
-    <>
+    // CartContext.Provider for React versions < 19
+    <CartContext>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -80,7 +82,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext>
   );
 }
 
